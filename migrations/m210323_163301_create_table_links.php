10b8abdaee462414c038eeb56958c0f5761e1843
denis <dprog103@gmial.ru>
2021-03-23 19:52:45 +0300

    migration

---
+++ migrations/m210323_163301_create_table_links.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210323_163301_create_table_links
+ */
+class m210323_163301_create_table_links extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m210323_163301_create_table_links cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210323_163301_create_table_links cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
